e9e39112fe873a3366ff2b2b42a5a1d95f6102dd
joobadam <joobadam@gmail.com>
2024-09-17 15:38:35 +0200

    fixed bugs

--- replace-env.js
+++ replace-env.js
@@ -1,4 +1,4 @@
-/* const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 
 const environmentFilePath = path.join(__dirname, 'dist', 'booknest', 'browser', 'main-*.js');
@@ -20,59 +20,4 @@ if (environmentFile) {
     console.log('Environment variables replaced successfully.');
 } else {
     console.error('Main bundle file not found.');
-} */
-
-
-
-const fs = require('fs');
-const path = require('path');
-
-const distDir = path.join(__dirname, 'dist', 'booknest', 'browser');
-const environmentFile = fs.readdirSync(distDir)
-  .find(file => file.startsWith('main-') && file.endsWith('.js'));
-
-if (!environmentFile) {
-    console.error('Main bundle file not found.');
-    process.exit(1);  
-}
-
-
-const filePath = path.join(distDir, environmentFile);
-let content;
-
-try {
-    content = fs.readFileSync(filePath, 'utf8');
-} catch (err) {
-    console.error('Failed to read the bundle file:', err);
-    process.exit(1);  
-}
-
-const envKeys = [
-    'FIREBASE_API_KEY',
-    'FIREBASE_AUTH_DOMAIN',
-    'FIREBASE_PROJECT_ID',
-    'FIREBASE_STORAGE_BUCKET',
-    'FIREBASE_MESSAGING_SENDER_ID',
-    'FIREBASE_APP_ID',
-    'FIREBASE_MEASUREMENT_ID',
-    'STRIPE_PUBLISHABLE_KEY'
-];
-
-envKeys.forEach(key => {
-    const value = process.env[key];
-    if (value) {
-        const regex = new RegExp(`('${key}':\\s*')([^']*)(')`, 'g');
-        content = content.replace(regex, `$1${value}$3`);
-        console.log(`Replaced ${key}`);
-    } else {
-        console.warn(`Environment variable ${key} is not set.`);
-    }
-});
-
-try {
-    fs.writeFileSync(filePath, content, 'utf8');
-    console.log('Environment variables replaced successfully.');
-} catch (err) {
-    console.error('Failed to write the bundle file:', err);
-    process.exit(1); 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-      apiKey: 'FIREBASE_API_KEY',
-      authDomain: 'FIREBASE_AUTH_DOMAIN',
-      projectId: 'FIREBASE_PROJECT_ID',
-      storageBucket: 'FIREBASE_STORAGE_BUCKET',
-      messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
-      appId: 'FIREBASE_APP_ID',
-      measurementId: 'FIREBASE_MEASUREMENT_ID'
+      apiKey: 'AIzaSyClH_9y9QWYFywa-wT-5C34S0WrVI-6PV4',
+      authDomain: 'booknest-2178a.firebaseapp.com',
+      projectId: 'booknest-2178a',
+      storageBucket: 'booknest-2178a.appspot.com',
+      messagingSenderId: '338625902172',
+      appId: '1:338625902172:web:1a682e3868a40bebee1899',
+      measurementId: 'G-D7HDZ5980K'
     },
-    stripePublishableKey: 'STRIPE_PUBLISHABLE_KEY'
+    stripePublishableKey: 'pk_test_51PzbcOLTmLTvxyZEo1iDYgUH7aoMOZR7WCxHHVJ1pWy5KSojXceOPfDBrE4AKv88HU04ApHpCnIu46vC9KUrZbP400Gik0Sg0y'
 };
+
