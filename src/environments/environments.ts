2748fcc4e2e796c08e36da6701c5b1659e9a6187
joobadam <joobadam@gmail.com>
2024-09-17 11:40:42 +0200

    fixed bugs

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,14 +1,15 @@
-export const environment = {
-    production: false,
-    firebaseConfig: {
-      apiKey: "AIzaSyClH_9y9QWYFywa-wT-5C34S0WrVI-6PV4",
-      authDomain: "booknest-2178a.firebaseapp.com",
-      projectId: "booknest-2178a",
-      storageBucket: "booknest-2178a.appspot.com",
-      messagingSenderId: "338625902172",
-      appId: "1:338625902172:web:1a682e3868a40bebee1899",
-      measurementId: "G-D7HDZ5980K"
-    },
-    stripePublishableKey: 'pk_test_51PzbcOLTmLTvxyZEo1iDYgUH7aoMOZR7WCxHHVJ1pWy5KSojXceOPfDBrE4AKv88HU04ApHpCnIu46vC9KUrZbP400Gik0Sg0y'
-};
+declare const process: any;
 
+export const environment = {
+  production: false, 
+  firebaseConfig: {
+    apiKey: process.env['FIREBASE_API_KEY'],
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+    projectId: process.env['FIREBASE_PROJECT_ID'],
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+    appId: process.env['FIREBASE_APP_ID'],
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID']
+  },
+  stripePublishableKey: process.env['STRIPE_PUBLISHABLE_KEY']
+};
