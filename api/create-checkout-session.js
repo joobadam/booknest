2726bd99c418a4d3dd74e118204d9708a1f2b1d3
joobadam <joobadam@gmail.com>
2024-09-18 15:08:27 +0200

    stripe

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,36 +1,26 @@
-const cors = require('cors');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-const corsOptions = {
-  origin: '*',
-  methods: ['POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
 
 module.exports = async (req, res) => {
   console.log('Function started');
   console.log('Request method:', req.method);
   console.log('Request headers:', JSON.stringify(req.headers));
   console.log('Request body:', JSON.stringify(req.body));
-  
+
  
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
   if (req.method === 'OPTIONS') {
-    console.log('Handling OPTIONS request');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.status(200).end();
-    console.log('OPTIONS request handled');
     return;
   }
 
-
-  await new Promise((resolve) => cors(corsOptions)(req, res, resolve));
-  console.log('CORS middleware applied');
-
   if (req.method === 'POST') {
-    console.log('Processing POST request');
     try {
       const { accommodationId, checkIn, checkOut, guests, totalPrice } = req.body;
       console.log('Parsed request body:', { accommodationId, checkIn, checkOut, guests, totalPrice });
